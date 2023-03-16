c132e9112b65248810c625089c4bcb3b8f6c855f
Hafiz Herla <72289760+HafizLinux@users.noreply.github.com>
2023-03-16 08:14:33 +0700

    first commit

---
+++ 4/kalkulator.js
@@ -0,0 +1,66 @@
+function tambah(){
+    var form = document.getElementById('kalkulator');
+    var a1 = parseFloat(form.angka1.value);
+    var a2 = parseFloat(form.angka2.value);
+
+    if (isNaN(a1) || isNaN(a2)) {
+        form.hasil.value = "Maaf angka belum terisi";
+    } else {
+        var total = a1 + a2;
+        form.hasil.value = total;
+    }
+}
+
+function kurang(){
+    var form = document.getElementById('kalkulator');
+    var a1 = parseFloat(form.angka1.value);
+    var a2 = parseFloat(form.angka2.value);
+
+    if (isNaN(a1) || isNaN(a2)) {
+        form.hasil.value = "Maaf angka belum terisi";
+    } else {
+        var total = a1 - a2;
+        form.hasil.value = total;
+    }
+}
+
+
+function kali(){
+    var form = document.getElementById('kalkulator');
+    var a1 = parseFloat(form.angka1.value);
+    var a2 = parseFloat(form.angka2.value);
+
+    if (isNaN(a1) || isNaN(a2)) {
+        form.hasil.value = "Maaf angka belum terisi";
+    } else {
+        var total = a1 * a2;
+        form.hasil.value = total;
+    }
+}
+
+
+function bagi(){
+    var form = document.getElementById('kalkulator');
+    var a1 = parseFloat(form.angka1.value);
+    var a2 = parseFloat(form.angka2.value);
+
+    if (isNaN(a1) || isNaN(a2)) {
+        form.hasil.value = "Maaf angka belum terisi";
+    } else {
+        var total = a1 / a2;
+        form.hasil.value = total;
+    }
+}
+
+function pangkat(){
+    var form = document.getElementById('kalkulator');
+    var a1 = parseFloat(form.angka1.value);
+    var a2 = parseFloat(form.angka2.value);
+
+    if (isNaN(a1) || isNaN(a2)) {
+        form.hasil.value = "Maaf angka belum terisi";
+    } else {
+        var total = Math.pow(a1, a2);
+        form.hasil.value = total;
+    }
+}
